5e7b9daadaebba71f987b96ee74246488aa4ac49
Tomas Petricek <tomas@tomasp.net>
2016-12-04 17:07:12 +0000

    Add Marvin

--- src/app.fsx
+++ src/app.fsx
@@ -19,89 +19,47 @@ let asm, debug =
   else IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), false
 let root = IO.Path.GetFullPath(IO.Path.Combine(asm, "..", "web"))
 
-// --------------------------------------------------------------------------------------
-// Loading content
-// --------------------------------------------------------------------------------------
-
 type SlackRequest =
-    {
-        Token       : string
-        TeamId      : string
-        TeamDomain  : string
-        ChannelId   : string
-        ChannelName : string
-        UserId      : string
-        UserName    : string
-        Command     : string
-        Text        : string
-        ResponseUrl : string
-    }
-    static member FromHttpContext (ctx : HttpContext) =
-        let get key =
-            match ctx.request.formData key with
-            | Choice1Of2 x  -> x
-            | _             -> ""
-        {
-            Token       = get "token"
-            TeamId      = get "team_id"
-            TeamDomain  = get "team_domain"
-            ChannelId   = get "channel_id"
-            ChannelName = get "channel_name"
-            UserId      = get "user_id"
-            UserName    = get "user_name"
-            Command     = get "command"
-            Text        = get "text"
-            ResponseUrl = get "response_url"
-        }
+  { Token : string
+    TeamId : string
+    TeamDomain : string
+    ChannelId : string
+    ChannelName : string
+    UserId : string
+    UserName : string
+    Command : string
+    Text : string
+    ResponseUrl : string }
 
-let sha512 (text : string) =
-    """
-    {
-    "response_type": "in_channel",
-    "text": "It's 80 degrees right now.",
-    "attachments": [
-        {
-            "text":"Partly cloudy today and tomorrow"
-        }
-    ]
-    }"""
+let parseRequest (req:HttpRequest) =
+  let get key =
+    match req.formData key with
+    | Choice1Of2 x -> x
+    | _ -> ""
+  { Token = get "token"
+    TeamId = get "team_id"
+    TeamDomain = get "team_domain"
+    ChannelId = get "channel_id"
+    ChannelName = get "channel_name"
+    UserId = get "user_id"
+    UserName = get "user_name"
+    Command = get "command"
+    Text = get "text"
+    ResponseUrl = get "response_url" }
 
-let sha512Handler =
-    fun (ctx : HttpContext) ->
-        (SlackRequest.FromHttpContext ctx
-        |> fun req ->
-            req.Text
-            |> sha512
-            |> Successful.OK) ctx
+let makeResponse =
+  sprintf "{ \"response_type\": \"in_channel\", \"text\": \"%s\" }" 
+
+let elizaHandler = request (fun req -> 
+  let question = parseRequest req
+  let answer = "You said " + question.Text
+  Successful.OK(makeResponse answer) )
 
 let app = 
   choose [
-    POST >=> path "/sha512" >=> Writers.addHeader "Content-type" "application/json" >=> sha512Handler 
-    Successful.OK "running" ]
-(*
-type SlackEvent = JsonProvider<"""{
-    "type": "url_verification" }""">
-
-type SlackHandshake = JsonProvider<"""{
-    "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
-    "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
-    "type": "url_verification" }""">
-
-let handleSlackEvent json =
-  printfn "SLACK MESSAGE!\n%s" json
-  match SlackEvent.Parse(json).Type with
-  | "url_verification" ->
-      Writers.addHeader "Content-type" "application/x-www-form-urlencoded"
-      >=> Successful.OK(SlackHandshake.Parse(json).Challenge)
-  | _ -> 
-      RequestErrors.BAD_REQUEST "bad request"
-
-let app = request (fun r ->
-  printfn "-----------------------\n%A" r
-  choose [
-    POST >=> request (fun r ->
-      let body = Text.UTF32Encoding.UTF8.GetString(r.rawForm)
-      handleSlackEvent body)
-    path "/" >=> Successful.OK "running"
-    Files.browse root ] )
-    *)
+    POST 
+      >=> path "/eliza" 
+      >=> Writers.addHeader "Content-type" "application/json" 
+      >=> elizaHandler
+    path "/" >=> Files.browseFile root "index.html"
+    Files.browse root ]
