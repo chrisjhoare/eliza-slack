b54ba5cf70c3ed99f42d73442ffe77871dd51060
Tomas Petricek <tomas@tomasp.net>
2016-12-04 17:12:50 +0000

    Simplify loader

--- build.fsx
+++ build.fsx
@@ -3,13 +3,18 @@
 // --------------------------------------------------------------------------------------
 
 #r "packages/FAKE/tools/FakeLib.dll"
+#r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#r "packages/Suave/lib/net40/Suave.dll"
 open Fake
 open System
 open System.IO
 open FSharp.Data
+open Suave
+open Suave.Operators
+open Suave.Web
+open Microsoft.FSharp.Compiler.Interactive.Shell
 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-let fsiPath = "packages/FSharp.Compiler.Tools/tools/fsiAnyCpu.exe"
 
 // --------------------------------------------------------------------------------------
 // For deployed run - compile as an executable
@@ -31,37 +36,65 @@ Target "build" (fun _ ->
 // For local run - automatically reloads scripts
 // --------------------------------------------------------------------------------------
 
-let startServers () = 
-  ExecProcessWithLambdas
-    (fun info -> 
-        info.FileName <- System.IO.Path.GetFullPath fsiPath
-        info.Arguments <- "--load:src/debug.fsx"
-        info.WorkingDirectory <- __SOURCE_DIRECTORY__)
-    TimeSpan.MaxValue false ignore ignore 
-
-Target "start" (fun _ ->
-  async { return startServers() } 
-  |> Async.Ignore
-  |> Async.Start
-
-  let mutable started = false
-  while not started do
-    try
-      use wc = new System.Net.WebClient()
-      started <- wc.DownloadString("http://localhost:8899/") |> String.IsNullOrWhiteSpace |> not
-    with _ ->
-      System.Threading.Thread.Sleep(1000)
-      printfn "Waiting for servers to start...."
-  traceImportant "Servers started...."
-  System.Diagnostics.Process.Start("http://localhost:8899/") |> ignore
-)
-
 Target "run" (fun _ ->
-  traceImportant "Press any key to stop!"
-  Console.ReadLine() |> ignore
-)
+  let sbOut = new Text.StringBuilder()
+  let sbErr = new Text.StringBuilder()
 
-"start" ==> "run"
+  let fsiSession =
+    let inStream = new StringReader("")
+    let outStream = new StringWriter(sbOut)
+    let errStream = new StringWriter(sbErr)
+    let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+    let argv = Array.append [|"/fake/fsi.exe"; "--quiet"; "--noninteractive" |] [||]
+    FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
+
+  let reportFsiError (e:exn) =
+    traceError "Reloading app.fsx script failed."
+    traceError (sprintf "Message: %s\nError: %s" e.Message (sbErr.ToString().Trim()))
+    sbErr.Clear() |> ignore
+
+  let reloadScript () =
+    try
+      traceImportant "Reloading 'app.fsx' script..." 
+      fsiSession.EvalInteraction(sprintf "#load @\"%s\"" (__SOURCE_DIRECTORY__ </> "src" </> "app.fsx"))
+      match fsiSession.EvalExpression("App.app") with
+      | Some app -> Some(app.ReflectionValue :?> WebPart)
+      | None -> failwith "Couldn't get 'app' value." 
+    with e -> reportFsiError e; None
+
+  let getLocalServerConfig port =
+    { defaultConfig with
+        homeFolder = Some __SOURCE_DIRECTORY__
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
+        bindings = [ HttpBinding.mkSimple HTTP  "127.0.0.1" port ] }
+
+  let mutable currentApp : WebPart = Successful.OK "Loading..."
+
+  let reloadAppServer (changedFiles: string seq) =
+    reloadScript () |> Option.iter (fun app -> 
+      currentApp <- app
+      traceImportant "Refreshed server." )
+
+  let port = 8899
+  let app = 
+    Writers.setHeader "Cache-Control" "no-cache, no-store, must-revalidate"
+    >=> Writers.setHeader "Pragma" "no-cache"
+    >=> Writers.setHeader "Expires" "0"
+    >=> fun ctx -> currentApp ctx
+  
+  let _, server = startWebServerAsync (getLocalServerConfig port) app
+
+  // Start Suave to host it on localhost
+  let sources = { BaseDirectory = __SOURCE_DIRECTORY__; Includes = [ "*.fs*" ]; Excludes = [] }
+  reloadAppServer sources
+  Async.Start(server)
+
+  // Watch for changes & reload when server.fsx changes
+  let watcher = sources |> WatchChanges (Seq.map (fun x -> x.FullPath) >> reloadAppServer)
+  traceImportant "Waiting for app.fsx edits. Press any key to stop."
+  System.Diagnostics.Process.Start("http://localhost:8899/") |> ignore
+  System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite)
+)
 
 // --------------------------------------------------------------------------------------
 // Azure - deploy copies the binary to wwwroot/bin
